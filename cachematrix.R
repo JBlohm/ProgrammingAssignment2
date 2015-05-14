5478162b61931e890f6e0c18208071a8be6b925b
JBlohm <joeblohm@gmail.com>
2015-05-14 11:37:35 +0200

    Filled cacheSolve function with life

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Module Description:
 ##
+## Module created 13-14. May 2015 by JBlohm
+##
 ## makeCacheMatrix: A function to create a matrix object that can cache its inverse
 ##
 ## cacheSolve: A function to compute the inverse of a matrix.
@@ -18,14 +20,19 @@ makeCacheMatrix <- function(x = matrix()) {
         ## Initialize the cache to NULL
         mcache <- NULL
         
-        ## SubFunctions to set/get the original matrix
+        ## SubFunction to set the original matrix
         setMatrix <- function(y) {
-                
+                x <<- y
+                mcache <<- NULL
         }
+        
+        ## SubFunction to get the original matrix
         getMatrix <- function() x
         
-        ##Sub functions to set/get the inverse to/from cache
+        ##Sub functions to store (set) the inverse to cache
         setInverse <- function(inverse) mcache <<- inverse
+        
+        ##Sub functions to retrieve (get) the inverse from cache
         getInverse <- function() mcache
         
         ## Return the list of functions defined above
@@ -36,7 +43,31 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Function description:
 ## cacheSolve: Return a matrix that is the inverse of 'x'
-## If possible, retreive the matrix from cache, else recalculate.
+## If possible, retreive the inverse matrix from cache, else recalculate.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## 'x' is a cacheMatrix object created with makeCacheMatrix()
+
+        ## Get the current cache of the cacheMatrix object
+        ## which could be an inverse of the original matrix,
+        ## or a NULL if the cache is invalid.
+        mcache <- x$getInverse()
+        
+        ## If the cache is valid, return the cached matrix.
+        if(!is.null(mcache)) {
+                message("returning cached data")
+                return(mcache)
+        }
+  
+        ## Cache is invalid if we get here, so we need to calculate the inverse
+        ## of the matrix.
+        
+        ## Get the matrix
+        mdata <- x$getMatrix()
+        ## solve it to get the inverse
+        minverse <- solve(mdata)
+        ## store result in cache
+        x$setInverse(minverse)
+        
+        ## Return the matrix that is the inverse of 'x'
+        minverse
 }
